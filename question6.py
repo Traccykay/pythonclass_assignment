9d0e0f34eb81c907c281d46245b66cb13dd159f7
Traccykay <traccykay@gmail.com>
2017-03-21 13:08:44 +0300

    Edditted Code

--- question6.py
+++ question6.py
@@ -1,7 +1,6 @@
 #Write a python program that merges and sorts two lists.
-def my_lists():
-	list1=[]
-	list2=[]
+list1=[1,2,3,4]
+list2=[5,5,6,2,4]
 
 merged_list=list1+list2
 print(sorted(merged_list))
